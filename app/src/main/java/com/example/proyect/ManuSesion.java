85f1692d777264540b4f3306196c9aaf34c51b2d
Jcogollo12 <josandres123@gmail.com>
2022-10-08 09:27:17 -0500

    recicleView

---
+++ app/src/main/java/com/example/proyect/ManuSesion.java
@@ -0,0 +1,126 @@
+package com.example.proyect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.proyect.data.Adapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Sesions;
+
+public class ManuSesion extends AppCompatActivity {
+    List<String> listado = new ArrayList<String>();
+    ArrayList<models.Sesions> listado2 = new ArrayList<Sesions>();
+    private ProgressBar progress;
+    private RecyclerView recicle;
+    private Adapter adapter;
+    private String texto;
+    Sesions sesions;
+    private boolean tem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manu_sesion);
+
+        progress = findViewById(R.id.progressBar);
+        recicle = findViewById(R.id.recyclerView);
+        new Task1().execute(listado.toString().trim());
+    }
+
+    public void searhFileAccedDefined(){
+        texto = "";
+        listado.clear();
+        try {
+            InputStream file = this.getResources().openRawResource(R.raw.salidalog);
+            InputStreamReader fileLoad = new InputStreamReader(file);
+            BufferedReader br = new BufferedReader(fileLoad);
+            String linea = br.readLine();
+            String todo = "";
+
+            while (linea != null) {
+                todo = todo + linea + "\n";
+                listado.add(linea.toString().split("] ")[0]);
+                listado.add(linea.toString().split("] ")[1]);
+                listado.add(linea.toString().split("] ")[2]);
+                listado.add(linea.toString().split("] ")[3]);
+                listado.add(linea.toString().split("] ")[4]);
+                sesions = new Sesions(listado.get(0).toString(), listado.get(1).toString(), listado.get(2).toString(), listado.get(3).toString(), listado.get(4).toString());
+                linea = br.readLine();
+                listado2.add(sesions);
+            }
+
+
+            br.close();
+            fileLoad.close();
+            texto = todo;
+
+
+
+
+
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this.getBaseContext(),e.getMessage(), Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+
+
+    }
+    class Task1 extends AsyncTask<String ,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            progress.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progress.setVisibility(View.INVISIBLE);
+           // searhFileAccedDefined();
+            Sesions  sesions = new Sesions("2022","Jose","1309","Test","Definido");
+            Sesions  sesions1 = new Sesions("2022","Wilson","1309","Test","Definido");
+            Sesions  sesions2 = new Sesions("2022","Andres","1309","Test","Definido");
+            Sesions   sesions3 = new Sesions("2022","Luis","1309","Test","Definido");
+            listado2.add(sesions);
+            listado2.add(sesions1);
+            listado2.add(sesions2);
+            listado2.add(sesions3);
+            adapter = new Adapter(getBaseContext(), listado2);
+            recicle.setAdapter(adapter);
+            recicle.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            try{
+
+                Thread.sleep(2000);
+
+            }catch (InterruptedException e){
+
+                e.printStackTrace();
+
+
+            }
+
+            return strings[0];
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/proyect/Menu.java
+++ app/src/main/java/com/example/proyect/Menu.java
@@ -16,13 +16,18 @@ public class Menu extends AppCompatActivity {
 
     //boton para conectar la pantalla menu con la pantalla incio
     public void Inicio(View View){
-        Intent Inicio = new Intent(this, MainActivity.class);
-        startActivity(Inicio);
+        Intent inicio = new Intent(this, MainActivity.class);
+        startActivity(inicio);
     }
 
     //Boton que conecta la pantalla menu con la pantalla Evento
     public void Evento(View View){
         Intent Evento = new Intent(this, MenuEventos.class);
         startActivity(Evento);
+    }
+    //Boton que conecta la pantalla menu con la pantalla Evento
+    public void Sesion(View View){
+       Intent Evento = new Intent(this, ManuSesion.class);
+         startActivity(Evento);
     }
 }
